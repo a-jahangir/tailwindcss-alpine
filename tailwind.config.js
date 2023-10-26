776e0865cd5af6be3c45150e3ba2d934af3b365d
amir hossein jahangir <a.jahangir74@gmail.com>
2023-10-26 18:26:02 +0330

    Final

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./*.html'],
+  darkMode: 'class',
+  theme: { 
+    fontFamily: {
+      vazir : ['Vazir']
+    },
+    extend: { 
+      lineHeight: {
+        '0': '0',
+       }
+    },
+  },
+  plugins: []
+}
